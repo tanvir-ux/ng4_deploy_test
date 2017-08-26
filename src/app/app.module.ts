da41e56bacfea63b9ac569c323027ee98c27359e
tanvir-ux <tanviralamshuvo@gmail.com>
2017-08-27 03:01:26 +0600

    ng4 deploy to github pages test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home',component: HomeComponent},
+      {path: 'product',component:ProductComponent},
+      {path: '',redirectTo: '/home',pathMatch: 'full'}
+      ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
